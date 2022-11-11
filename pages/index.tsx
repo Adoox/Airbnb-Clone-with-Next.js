caee1958a69dc4decc65344eb90137b21ee4a708
Adoox <adnankubat40@gmail.com>
2022-11-11 19:03:26 +0100

    implemented banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
-    <div className="">
+    <div>
       <Head>
         <title>Airbnb Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header />
+      <Banner />
     </div>
   );
 };
