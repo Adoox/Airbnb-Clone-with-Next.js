2ce4bfcf84c566ea2b7325d9ce23490a11af934e
Adoox <adnankubat40@gmail.com>
2022-11-15 00:22:12 +0100

    fixed bug with converting date

--- components/Header.tsx
+++ components/Header.tsx
@@ -38,8 +38,8 @@ export default function Header() {
       pathname: "/search",
       query: {
         location: searchInput,
-        startDate: startDate.toISOString(),
-        endDate: endDate.toISOString(),
+        startDate: startDate.toString(),
+        endDate: endDate.toString(),
         numberOfGuests: guestsNumber,
       },
     });
